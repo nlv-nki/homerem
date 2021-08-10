c2c0ffad6e6b7f405594c6f1b4d7f3ad2b985183
Nikolai <nlv.nki@gmail.com>
2021-08-10 14:37:08 +0300

    fix gulp task

--- gulp-tasks/webp.js
+++ gulp-tasks/webp.js
@@ -9,9 +9,11 @@ import newer from "gulp-newer";
 import debug from "gulp-debug";
 import browsersync from "browser-sync";
 import yargs from "yargs";
+var log = require("fancy-log");
 
 const argv = yargs.argv,
     production = !!argv.production;
+log(production);
 
 gulp.task("webp", () => {
     return gulp.src(paths.images.src)
@@ -27,3 +29,5 @@ gulp.task("webp", () => {
         }))
         .on("end", browsersync.reload);
 });
+log(paths.images.dist);
+
